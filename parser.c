0fbf3508da9075e8cfb2b8e61207c8e31b771856
Kelvin Lee <kiyolee@gmail.com>
2023-10-29 09:46:52 +1100

    Fix code to compile

--- parser.c
+++ parser.c
@@ -8896,7 +8896,7 @@ static int
 xmlAttrHashInsert(xmlParserCtxtPtr ctxt, unsigned size, const xmlChar *name,
                   const xmlChar *uri, unsigned hashValue, int aindex) {
     xmlAttrHashBucket *table = ctxt->attrHash;
-    xmlAttrHashBucket *bucket;
+    xmlAttrHashBucket *bucket = NULL;
     unsigned hindex;
 
     hindex = hashValue & (size - 1);
@@ -9372,7 +9372,7 @@ next_attr:
 	if (defaults != NULL) {
 	    for (i = 0; i < defaults->nbAttrs; i++) {
                 xmlDefAttr *attr = &defaults->attrs[i];
-                const xmlChar *nsuri;
+                const xmlChar *nsuri = NULL;
                 unsigned hashValue, uriHashValue;
                 int res;
 
