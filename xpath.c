c6fdd2a3e249fd4ca8ded6f0f62646ff3506e602
Kelvin Lee <kiyolee@gmail.com>
2024-01-07 13:33:47 +1100

    Update following upstream, current version is 2.12.3

--- xpath.c
+++ xpath.c
@@ -7919,7 +7919,7 @@ void
 xmlXPathLangFunction(xmlXPathParserContext *ctxt, int nargs) {
     xmlXPathObjectPtr val;
     xmlNodePtr cur;
-    xmlChar *theLang;
+    xmlChar *theLang = NULL;
     const xmlChar *lang;
     int ret = 0;
     int i;
