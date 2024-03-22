14cb76f3990416b1b63d26be316360de83fe2951
Kelvin Lee <kiyolee@gmail.com>
2024-03-22 14:43:36 +1100

    Fix code to compile

--- tree.c
+++ tree.c
@@ -5258,7 +5258,7 @@ xmlNodeSetName(xmlNodePtr cur, const xmlChar *name) {
  */
 int
 xmlNodeSetBase(xmlNodePtr cur, const xmlChar* uri) {
-    xmlNsPtr ns;
+    xmlNsPtr ns = NULL;
     xmlChar* fixed;
 
     if (cur == NULL)
@@ -6070,7 +6070,7 @@ xmlSearchNsSafe(xmlNodePtr node, const xmlChar *prefix,
 xmlNsPtr
 xmlSearchNs(xmlDocPtr doc ATTRIBUTE_UNUSED, xmlNodePtr node,
             const xmlChar *nameSpace) {
-    xmlNsPtr cur;
+    xmlNsPtr cur = NULL;
 
     xmlSearchNsSafe(node, nameSpace, &cur);
     return(cur);
@@ -6226,7 +6226,7 @@ xmlSearchNsByHrefSafe(xmlNodePtr node, const xmlChar *href,
 xmlNsPtr
 xmlSearchNsByHref(xmlDocPtr doc ATTRIBUTE_UNUSED, xmlNodePtr node,
                   const xmlChar * href) {
-    xmlNsPtr cur;
+    xmlNsPtr cur = NULL;
 
     xmlSearchNsByHrefSafe(node, href, &cur);
     return(cur);
