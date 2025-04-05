457c7094602052510c8ebbcad38ec3b35fb290d3
Kelvin Lee <kiyolee@gmail.com>
2025-04-05 15:23:07 +1100

    Suppress C4056, C4723, and C4756 properly

--- xmlschemastypes.c
+++ xmlschemastypes.c
@@ -515,6 +515,16 @@ xmlSchemaCleanupTypesInternal(void) {
     /* Note that the xmlSchemaType*Def pointers aren't set to NULL. */
 }
 
+#ifdef _MSC_VER
+#pragma warning(push)
+#if defined(NAN) && defined(INFINITY)
+#pragma warning(disable: 4056) // overflow in floating-point constant arithmetic
+#pragma warning(disable: 4756) // overflow in constant arithmetic
+#else
+#pragma warning(disable: 4723) // potential divide by 0
+#endif
+#endif
+
 /*
  *
  * Initialize the default XML Schemas type library
@@ -537,16 +547,9 @@ xmlSchemaInitTypes(void)
     xmlSchemaPINF = INFINITY;
     xmlSchemaNINF = -INFINITY;
 #else
-#ifdef _MSC_VER
-#pragma warning(push)
-#pragma warning(disable: 4723) // potential divide by 0
-#endif
     xmlSchemaNAN = 0.0 / zero;
     xmlSchemaPINF = 1.0 / zero;
     xmlSchemaNINF = -xmlSchemaPINF;
-#ifdef _MSC_VER
-#pragma warning(pop)
-#endif
 #endif
 
     xmlSchemaTypesBank = xmlHashCreate(40);
@@ -869,6 +872,10 @@ error:
     xmlSchemaCleanupTypesInternal();
     return (-1);
 }
+
+#ifdef _MSC_VER
+#pragma warning(pop)
+#endif
 
 /**
  * @deprecated This function will be made private. Call xmlCleanupParser
